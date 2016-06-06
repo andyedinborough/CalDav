fe66d7ddab2d1164fe7ad8f03f4bc781c8039335
altostratous <altostratous@gmail.com>
2016-06-06 22:42:55 +0430

    Removed unnassary interfaces had been added.

--- CalDav/IConnection.cs
+++ CalDav/IConnection.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
-namespace CalCli.API
+namespace CalDav
 {
-    interface ICalendar
+    public interface IConnection
     {
+        WebRequest Authorize(WebRequest request);
     }
 }
